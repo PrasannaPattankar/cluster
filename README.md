cf2eac3702f82d377cb07b8db389d5531675de22
PrasannaPattankar <prasanna.pattankar@gmail.com>
2021-08-16 19:14:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+In the root directory, you can run:
+
+### `npm start`
+
+Website will be running in browser <br>
+Open [http://localhost:3000](http://localhost:3000).
+
+
---
+++ src/api/dataService.js
@@ -0,0 +1,105 @@
+
+
+export default function() {
+  // Resloving Dummy Data. 
+  return Promise.resolve(
+    [
+        {
+          custid: 1,
+          name: "Amit",
+          amt: 120,
+          transactionDt: "10-03-2021"
+        },
+        {
+          custid: 1,
+          name: "Amit",
+          amt: 75,
+          transactionDt: "05-21-2021"
+        },
+        {
+          custid: 1,
+          name: "Amit",
+          amt: 94,
+          transactionDt: "05-21-2021"
+        },
+        {
+          custid: 1,
+          name: "Amit",
+          amt: 10,
+          transactionDt: "06-01-2021"
+        },
+        {
+          custid: 1,
+          name: "Amit",
+          amt: 75,
+          transactionDt: "06-21-2021"
+        },
+        {
+          custid: 1,
+          name: "Amit",
+          amt: 200,
+          transactionDt: "07-01-2021"
+        },
+        {
+          custid: 1,
+          name: "Amit",
+          amt: 1,
+          transactionDt: "07-04-2021"
+        },
+        {
+          custid: 1,
+          name: "Amit",
+          amt: 80,
+          transactionDt: "07-03-2021"
+        },
+        {
+          custid: 1,
+          name: "Amit",
+          amt: 224,
+          transactionDt: "07-21-2021"
+        },
+        {
+          custid: 2,
+          name: "Amy",
+          amt: 125,
+          transactionDt: "05-01-2021"
+        },
+        {
+          custid: 2,
+          name: "Amy",
+          amt: 75,
+          transactionDt: "05-21-2021"
+        },
+        {
+          custid: 2,
+          name: "Amy",
+          amt: 10,
+          transactionDt: "06-01-2021"
+        },
+        {
+          custid: 2,
+          name: "Amy",
+          amt: 75,
+          transactionDt: "06-21-2021"
+        },
+        {
+          custid: 2,
+          name: "Amy",
+          amt: 200,
+          transactionDt: "07-01-2021"
+        },
+        {
+          custid: 2,
+          name: "Amy",
+          amt: 224,
+          transactionDt: "07-21-2021"
+        },
+        {
+          custid: 3,
+          name: "Test Case1",
+          amt: 120,
+          transactionDt: "06-21-2021"
+        }
+    ]
+  );
+};
